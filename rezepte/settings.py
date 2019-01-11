8ce6397d4551ebdc7a8e51cb886bfbbe518dc3e8
raphi <raphi.rm@gmail.com>
2019-01-11 11:37:14 +0100

    Make Production Ready Build

--- rezepte/settings.py
+++ rezepte/settings.py
@@ -24,8 +24,13 @@ SECRET_KEY = '#(&2r77)wqbcq#n=1m9zaj28@u6&68p3_+v66rg-&e$zak56t8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+if os.getenv('DJANGO_ENV') == 'prod':
+    DEBUG = False
+    ALLOWED_HOSTS = ['rezepte.marques.pw', '10.42.24.5', '127.0.0.1']
+else:
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['rezepte.marques.pw', '10.42.24.5', '127.0.0.1']
 
 
 # Application definition
@@ -78,13 +83,24 @@ WSGI_APPLICATION = 'rezepte.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.getenv('DJANGO_ENV') == 'prod':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'rezepte',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
