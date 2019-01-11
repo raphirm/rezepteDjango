e23e9e3a65d3786f3f745bcfc0cf188a9aea7d44
raphi <raphi.rm@gmail.com>
2019-01-11 11:05:26 +0100

    Change recipes app to root

--- rezepte/urls.py
+++ rezepte/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
-    path('recipes/', include('recipes.urls')),
+    path('/', include('recipes.urls')),
     path('admin/', admin.site.urls),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
